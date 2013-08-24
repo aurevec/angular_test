ea056a5e355cad3285d915c6f803024447d7a4f2
Aurelien Vecchiato <aurelien.vecchiato@belighted.com>
2013-08-24 21:37:31 +0200

    first commit

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :first_name, :last_name, :phone, :mobile, :email, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angular::Application.config.secret_key_base = 'cd885ae0e24a7fc7200ac80439f67f451ec9bb555ac48fc6338d1af4cc8d0cf2d32f6524aa97e626e988b87976596f6d1ea3f39ba0b12eda4e85a7209c122830'
